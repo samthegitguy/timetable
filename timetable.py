5acc2b04ee9eeabe8c28c792a1dbf5549308fb99
samthegitguy <pokemonexpok@gmail.com>
2019-10-15 05:20:01 +0000

    Oh, just another commit, still not working

--- timetable.py
+++ timetable.py
@@ -1,13 +1,14 @@
 #This program is used to learn timetables. below are improts of modules
 import sys, random
-#Function Def
-def checkanswer(x,y)
-    playersays = input("What is " + x + " plus " + y + " ?")
+#Function Definitions
+def checkanswer(x ,y ):
+    playersays = input()
     if x * y == playersays:
         print ("Correct!")
         score += 1
-    else print ("Oops! That's incorrect")
-def menu (returning)
+    else:
+	print ("That's incorrect")
+def menu (returning):
     if returning == True:
         print ("Your current score is " + score + ". Let's try and beat it!")
     timetable = input ("What table will you practice? ")
@@ -17,7 +18,7 @@ score = 0
 Returning = False
 table = menu (Returning)
 for i in range (1,10): 
-    checkanswer (x, table)
+    checkanswer (i, table)
     i = i + 1
 Returning = True
 
