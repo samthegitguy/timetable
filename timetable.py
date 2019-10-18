60750b9ad4ca0fbf89fc32072a08ee2735e252b6
samthegitguy <pokemonexpok@gmail.com>
2019-10-18 23:42:56 +0000

    FIRST WORKING VERSION!!!! After a long time, we finally reach our first working commit. I shall make a new release soon. The problem was that... I don't know. I'll have to check github later, which lines I added to make it work.

--- timetable.py
+++ timetable.py
@@ -2,32 +2,30 @@
 #This program was developed by Sam
 #It is used to learn timetables. it asks you to choose a timetable to quiz you on, and it asks all the relevant questions, and reports on your success,
 #/ failure, and adds score accordingly.
+#Detailed description can be found in readme.txt
 #these are module importations.
-import sys, random, 
+import sys, random
 #Function Definitions
-def checkanswer(x ,y ):
-    playersays = input()
+def checkanswer(x ,y,thescore):
+    playersays = input("What is " + str(x) + " times " + str(y) + " ? ")
     if x * y == playersays:
         print ("Correct!")
-        score += 1
+        thescore += 1
     else:
 	print ("That's incorrect")
-def menu (returning):
-    if returning == True:
-        print ("Your current score is " + score + ". Let's try and beat it!")
+    return thescore
+def menu ():
+    print ("Your current score is " + str(score) + ". Let's try and beat it!")
     timetable = input ("What table will you practice? ")
     return timetable
 #here is main program
 i = 1
 score = 0
-Returning = False
-table = menu (Returning)
+table = menu ()
 #main question loop
 for i in range (1,10): 
-    checkanswer (i, table)
+    score = checkanswer (i, table, score)
     i = i + 1
-#tells the menu to display score next time round
-    Returning = True
-
+print("Your Score Is..." + str(score) + " ! ")
 
 
